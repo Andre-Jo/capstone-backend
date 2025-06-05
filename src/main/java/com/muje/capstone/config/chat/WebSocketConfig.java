80e761a78493d533f8ce6916c937090136ee246f
andre <andre1101@naver.com>
2025-06-06 02:54:48 +0900

    Front Ws 연결

--- src/main/java/com/muje/capstone/config/chat/WebSocketConfig.java
+++ src/main/java/com/muje/capstone/config/chat/WebSocketConfig.java
@@ -19,8 +19,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/api/ws")
-                .setAllowedOrigins(frontendBaseUrl)
-                .withSockJS();
+                .setAllowedOrigins(frontendBaseUrl);
+//                .withSockJS();
     }
 
     @Override
